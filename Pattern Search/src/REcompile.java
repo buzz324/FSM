aa6444ba1eeef4977e9eb437c200b5b7c4a891d2
buzz <ejkho24@gmail.com>
2021-05-28 16:37:50 +1200

    From lecture(psudocode)

--- Pattern Search/src/REcompile.java
+++ Pattern Search/src/REcompile.java
@@ -8,6 +8,9 @@ public class REcompile {
     // and two numbers indicating the two possible next states.
 
     String p = "ab*a+cd"; //globally accessible variable
+
+
+
     int j =0;
 
     //find an expression (term, E)
@@ -15,20 +18,59 @@ public void expression(){
 
     term();//find the term first
 
+    //check if this is an end of string
+    if(p[j]==null){
+        //return great
+    }
+
+    //Check if the expression is looking at ( or literal after term ----look ahead
+    if(p[j]!="("&&isVocab(p[j])){
+
+        //return bad news
+
+    }
+    expression();
+
 }
 
-//Find a tern
+//T -> F or F* or F+T
 public void term(){
 
-    //find a factor
-    factor();
+    //First execution term will take will be factor, if not F? false or exception etc
+    if (factor()){
+        if(p[j]=="*"){
+            j++;
+            //return true/success
+        }
+        if(p[j]=="+"){
+            j++;
+            if(term()){
+
+            }
+                //true return
+        }
+    }
 }
 
+
+//F -> literal or expression
     private void factor() {
+
+    //Check if looking at literals
     if (isVocab(p[j])){
         j++;
     }else {
 
+        //Check if it is an expression
+        if(p[j]=="("){
+            j++;
+            expression();
+        }
+        if (p[j]==")"){
+            j++;
+            //return true, success
+
+        }
     }
     }
 
@@ -36,4 +78,19 @@ public void term(){
 public boolean isVocab(char c){
     return false;//if open bracket
 }
+
+
+
+
+    public static void main(String[] args) {
+
+
+        String st = "ab*a+cd";
+        String [] p=new String[9];
+        for (int i=0; i < p.length; i++)
+
+            System.out.println("Char " + i + " is " + st.charAt(i));
+    }
+
+
 }
