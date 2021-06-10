9b995cc1222cc05b8edc085fa9caac62e3843b04
buzz <ejkho24@gmail.com>
2021-06-10 20:40:45 +1200

    Compile (+ operator)

--- Pattern Search/src/REcompile.java
+++ Pattern Search/src/REcompile.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import java.util.ArrayList;
+
 public class REcompile {
 
 
@@ -7,11 +10,12 @@ public class REcompile {
     // the symbol to be matched or branch-state indicator (or other type of indicator if you find an alternative useful),
     // and two numbers indicating the two possible next states.
 
-    //String p = "a*bB*"; //globally accessible variable
+    //String p = "a*b*"; //globally accessible variable
     //String p = "ab*a+cd"; //globally accessible variable
+    String p = "a+c+d"; //globally accessible variable
 
-    String p = "ab"; //globally accessible variable
-    //String p = "\\aabb"; //globally accessible variable
+    //String p = "absd"; //globally accessible variable
+    //String p = "\\3aa"; //globally accessible variable
 
     //Global state variable, for setting up a new state
     int state= 0;
@@ -25,6 +29,7 @@ public class REcompile {
     //Character array
     char [] character = new char[lengthOfString+2];
     //next state arrays
+    //ArrayList<Integer> n1 = new ArrayList<Integer>(p.length()+2);
     int [] n1 = new int[lengthOfString+2];
     int [] n2 = new int [lengthOfString+2];
 
@@ -54,7 +59,11 @@ public class REcompile {
 
         int result;
 
-        //set
+        //While state is not reaching to the end of array size, and character counter of string never reaches the end, then keep adding states
+        if (state<stringLength()+1&&j<p.length())
+        {
+
+        //set start state
         if(state==0){
             setState(state,'`',1,1);
 
@@ -69,11 +78,12 @@ public class REcompile {
         //Set a state only if we have a space in the array
         if (j < character.length+1) {
 
-            //Case with "\": anything comes after "\", treat as a literal
+
+            //Case with "\": anything comes after "\", treat as a literalhow to get elements from arraylist in java
             if (p.charAt(j) == '\\') {
 
                 //Set a state with any character after "\"
-                setState(state, p.charAt(j+1), state + 2, state + 2);
+                setState(state, p.charAt(j+1), state + 1, state + 1);
                 //@Testing
                 System.out.println("Current state with '\\': "+state+", ch= "+ character[state]+ ", n1: "+n1[state]+", n2: "+n2[state]);
                 //Increment since a state has been created
@@ -86,7 +96,6 @@ public class REcompile {
             if (isVocab(p.charAt(j))) {
 
 
-
                 setState(state, p.charAt(j), state+1, state+1);
                 //@Testing
                 System.out.println("Current state adding normal literal: "+state+", ch= "+ character[state]+ ", n1: "+n1[state]+", n2: "+n2[state]);
@@ -94,16 +103,6 @@ public class REcompile {
                 j++; result = state; state++;
 
 
-                //End of array
-                if (state==stringLength()+1) {
-                    //Final state to end
-                    setState(state, 'F', -1, -1);
-                    //@Testing
-                    System.out.println("End state: "+state+", ch= "+ character[state]+ ", n1: "+n1[state]+", n2: "+n2[state]);
-                    result=state;
-                    return result;
-
-                }
                 return result;
 
             } /*else {
@@ -127,7 +126,22 @@ public class REcompile {
             }*/
 
         }
-            return state;//No factor so return start state
+    } //End of array so set final state
+
+
+        if (j==stringLength()) {
+
+            //Final state to end
+            setState(state, 'F', -1, -1);
+            //@Testing
+            System.out.println("End state: "+state+", ch= "+ character[state]+ ", n1: "+n1[state]+", n2: "+n2[state]);
+            result=state;
+            j++;state++;
+            return result;
+
+        }
+
+        return state-1;//No factor so return start state
 
     }
 
@@ -137,43 +151,66 @@ public class REcompile {
 //T -> F or F* or F+T
 public int term(){
 
+
     //First execution term will take will be factor, if not F? false or exception etc
     int t1= factor();
     int result=t1;
+
+
     int f1= state-1;//last one just built from factor()
 
-  /*  if(p.charAt(j)=='*'){
+    if (j<p.length()) {
+        if (p.charAt(j) == '*') {
 
-            setState(state,'`',state-1,state+1);//branching that 0 or more of t1
-            System.out.println("Current state with closure symbol: "+state+", ch= "+ character[state]+ ", n1: "+n1[state]+", n2: "+n2[state]);
+            setState(state, '`', state - 1, state + 1);//branching that 0 or more of t1
+            System.out.println("Current state with closure symbol: " + state + ", ch= " + character[state] + ", n1: " + n1[state] + ", n2: " + n2[state]);
 
-            j++; result= state; state++;//increment for the next state
+            j++;
+            result = state;
+            state++;//increment for the next state
             return result;
 
-        }*/
+        }
 
-        /*
-        if(character[j]=='+'){
-            int t2=term();
+        if(p.charAt(j)=='+'){
+
+            j++;//move onto next symbol after the "+"
+
+            //if there is no next character after "+"
+            if(!isVocab(p.charAt(j))){
+                //one or more time of previous symbols ex:a**
+                //NEED CODES
+            }
+
+            int t2=term(); //Remember the previous state to join
+
+            //Set the disjunction branch
             setState(state,'`',t1,t2);
+            System.out.println("Current state with disjunction symbol: " + state + ", ch= " + character[state] + ", n1: " + n1[state] + ", n2: " + n2[state]);
+
+            //set the previous state from disjunction regex to disjunction branch
+            setState(f1-1,character[f1-1],state,state);
+
             result=state;
-            j++;
+
 
             //Check if preceding term is non-branching state by comparing two next states
             if(n1[f1]==n2[f1]){
                 //Updating the preceding term's states
                 setState(f1,character[f1],state,state);
-                state++;
+
+
             }else {
                 //Branching so only one state to merge
-                setState(f1,character[f1],n1[f1],state);
-                state++;
+                setState(f1,character[f1+1],n1[f1],state);
+
             }
+            state++;
 
             return result;
-        }*/
+        }
         //Factor itself
-
+    }
     return result;
 }
 
@@ -199,5 +236,12 @@ public int term(){
     return
 }*/
 
+
+    public void dump(){
+        for (int i = 0; i<character.length;i++){
+            System.out.println("State: "+i+" ch is "+character[i]+" n1 is: "+n1[i]+" n2 is:"+n2[i]);
+        }
+
+    }
 
 }
