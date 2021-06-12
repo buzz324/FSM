b253d30effd2df9a3705555af4f774fd1bd4dbe4
buzz <ejkho24@gmail.com>
2021-06-13 00:50:58 +1200

    Done

--- Pattern Search/src/REcompile.java
+++ Pattern Search/src/REcompile.java
@@ -1,56 +1,67 @@
-import java.awt.*;
 import java.util.ArrayList;
+
 
 public class REcompile {
 
+    public static String p = "";
+    public static String[] character;
+    public static int[] n1;
+    public static int[] n2;
 
-    // The first of these must accept a regexp pattern as a command-line argument
-    // (enclosed within quotes—see "Note" below), and produce as standard output a description of the corresponding FSM,
-    // such that each line of output includes four things: the state-number,
-    // the symbol to be matched or branch-state indicator (or other type of indicator if you find an alternative useful),
-    // and two numbers indicating the two possible next states.
+    public static void main(String[] args) {
 
-    //String p = "a*b*"; //globally accessible variable
-    //String p = "ab*a+cd"; //globally accessible variable
-    String p = "a+c"; //globally accessible variable
 
-    //String p = "absd"; //globally accessible variable
-    //String p = "\\3aa"; //globally accessible variable
+        p = args[0];
+
+        //Array length is string length + start&end state
+        character = new String[p.length() + 2];
+        n1 = new int[p.length() + 2];
+        n2 = new int[p.length() + 2];
+
+        REcompile complie = new REcompile();
+
+        complie.output(p);
+
+    }
+
 
     //Global state variable, for setting up a new state
-    int state= 0;
-    int finalState;
-    String alternation="+";
-    String closure="()";
-    String concatenation = "*";
+    int state = 0;
 
 
-    int lengthOfString = p.length();
-    //Character array
-    char [] character = new char[lengthOfString+2];
-    //next state arrays
-    //ArrayList<Integer> n1 = new ArrayList<Integer>(p.length()+2);
-    int [] n1 = new int[lengthOfString+2];
-    int [] n2 = new int [lengthOfString+2];
+    //Receive String from terminal and set FSM
+    public void output(String regex) {
+
+        REcompile compile = new REcompile();
+
+        int initial ;
+        for (int i = 0; i < regex.length() + 2; i++) {
+            initial=compile.expression();
+
+        }
+        compile.dump();
+    }
+
+
 
     //Increment counter for regexp FSM
-    int j =0;
+    int j = 0;
 
     //Set up a state for regexp
-    public void setState(int globalstate,char ch, int nextState, int nextState2){
-        character[globalstate]=ch;
-        n1[globalstate]=nextState;
-        n2[globalstate]=nextState2;
+    public void setState(int globalstate, String ch, int nextState, int nextState2) {
+        character[globalstate] = ch;
+        n1[globalstate] = nextState;
+        n2[globalstate] = nextState2;
     }
 
     //Created for testing
-    public int stringLength(){
+    public int stringLength() {
         return p.length();
     }
 
     //Check the character is allowed symbol
-    public boolean isVocab(char ch){
-        if( ch == '+' || ch == '*' ||ch=='?'||ch=='|' )return false;
+    public boolean isVocab(char ch) {
+        if (ch == '+' || ch == '*' || ch == '?' || ch == '|' || ch == '[' || ch == '(') return false;
         return true;
     }
 
@@ -60,195 +71,252 @@ public class REcompile {
         int result;
 
         //While state is not reaching to the end of array size, and character counter of string never reaches the end, then keep adding states
-        if (state<stringLength()+1&&j<p.length())
-        {
+        if (state < stringLength() + 1 && j < p.length()) {
 
-        //set start state
-        if(state==0){
-            setState(state,'`',1,1);
+            //set start state
+            if (state == 0) {
+                setState(state, "Start", 1, 1);
 
-            //@Testing
-            System.out.println("Start state: "+state+", ch= "+ character[state]+ ", n1: "+n1[state]+", n2: "+n2[state]);
-
-            result=state; state++;
-            return result;
-
-        }
-
-        //Set a state only if we have a space in the array
-        if (j < character.length+1) {
-
-
-            //Case with "\": anything comes after "\", treat as a literalhow to get elements from arraylist in java
-            if (p.charAt(j) == '\\') {
-
-                //Set a state with any character after "\"
-                setState(state, p.charAt(j+1), state + 1, state + 1);
-                //@Testing
-                System.out.println("Current state with '\\': "+state+", ch= "+ character[state]+ ", n1: "+n1[state]+", n2: "+n2[state]);
-                //Increment since a state has been created
-                j+=2; result = state; state++;
-
+                result = state;
+                state++;//increment for the next state
                 return result;
+
             }
 
-            //Check if looking at a literal (concatenation)
-            if (isVocab(p.charAt(j))) {
+            //Set a state only if we have a space in the array
+            if (j < character.length + 1) {
 
 
-                setState(state, p.charAt(j), state+1, state+1);
-                //@Testing
-                System.out.println("Current state adding normal literal: "+state+", ch= "+ character[state]+ ", n1: "+n1[state]+", n2: "+n2[state]);
-                //Increment since a state has been created
-                j++; result = state; state++;
+                //Case with "\": anything comes after "\", treat as a literal how to get elements from arraylist in java
+                if (p.charAt(j) == '\\') {
 
+                    //Set a state with any character after "\"
+                    setState(state, String.valueOf(p.charAt(j + 1)), state + 1, state + 1);
+                    j += 2;
+                    result = state;
+                    state++;//increment for the next state
 
-                return result;
+                    return result;
+                }
 
-            } /*else {
+                //Check if looking at a literal (concatenation)
+                if (isVocab(p.charAt(j))) {
 
-                //Check if it is an expression
-                if (p.charAt(j) == '(') {
+                    setState(state, String.valueOf(p.charAt(j)), state + 1, state + 1);
                     j++;
+                    result = state;
+                    state++;//increment for the next state
 
-                    //@@NEED MODIFICATION
-                    //result=expression();
-
-                    if (p.charAt(j) == ')') {
-                        j++;
-                    } else {
-                        System.out.println("Must have a ')' in the end");
-                    }
+                    return result;
 
                 } else {
-                    System.out.println("not a factor");
+
+                    //Check if it is an expression
+                    if (String.valueOf(p.charAt(j)).equals("(")) {
+                        j++;
+
+                        result = expression();
+
+                        if (String.valueOf(p.charAt(j)).equals(")")) {
+                            j++;
+                        } else {
+                            System.err.println("Must have a ')' in the end");
+                        }
+
+                    } else {
+                        System.err.println("not a factor");
+                    }
                 }
-            }*/
 
-        }
-    } //End of array so set final state
+            }
+        } //End of array so set final state
 
+        //Final state
+        if (j == stringLength()) {
 
-        if (j==stringLength()) {
-
-            //Final state to end
-            setState(state, 'F', -1, -1);
-            //@Testing
-            System.out.println("End state: "+state+", ch= "+ character[state]+ ", n1: "+n1[state]+", n2: "+n2[state]);
-            result=state;
-            j++;state++;
+            setState(state, "End", -1, -1);
+            result = state;
+            j++;
+            state++;
             return result;
-
         }
 
-        //nothing matches so increment(for "+", operator)
-        // j++;
-
-        return state-1;//No factor so return start state
-
+        //No factor so return start state
+        return state - 1;
     }
 
+    //T -> F or F* or F+T
+    public int term() {
 
+        //First execution term will take will be factor, if not F? false or exception etc
+        int t1 = factor();
+        int result = t1;
 
+        int f1 = state - 1;//last one just built from factor()
 
-//T -> F or F* or F+T
-public int term(){
+        if (j < p.length()) {
+            if (String.valueOf(p.charAt(j)).equals("*")) {
 
+                setState(state, "BR", state - 1, state + 1);//branching that 0 or more of t1
+                j++;
+                result = state;
+                state++;//increment for the next state
 
-    //First execution term will take will be factor, if not F? false or exception etc
-    int t1= factor();
-    int result=t1;
+                return result;
 
+            } else if (String.valueOf(p.charAt(j)).equals("+")) {
 
-    int f1= state-1;//last one just built from factor()
+                //Set branch state for "+"
+                setState(state, "BR", state - 1, state + 1);
+                j++;
+                result = state;
+                state++;//increment for the next state
 
-    if (j<p.length()) {
-        if (p.charAt(j) == '*') {
+                return result;
 
-            setState(state, '`', state - 1, state + 1);//branching that 0 or more of t1
-            System.out.println("Current state with closure symbol: " + state + ", ch= " + character[state] + ", n1: " + n1[state] + ", n2: " + n2[state]);
+            }
+            //the preceding regexp can occur zero or one time
+            else if (String.valueOf(p.charAt(j)).equals("?")) {
 
-            j++;
-            result = state;
-            state++;//increment for the next state
-            return result;
+                //Set a branch state for "?"
+                setState(state, "BR", state - 1, state + 1);
 
-        }
+                //Update preceding symbol
 
-        if(p.charAt(j)=='+'){
+                if (n1[t1] == n2[t1]) {
+                    //for non-branching state
+                    setState(t1, character[t1], state, state);
 
-            j++;//move onto next symbol after the "+"
+                } else {
+                    //for branching state
+                    setState(t1, character[t1], n1[t1], state);
 
-            //if there is no next character after "+"
-        /*    if(!isVocab(p.charAt(j))){
-                //one or more time of previous symbols ex:a**
-                //NEED CODES
-            }*/
+                }
+                j++;
+                result = state;
+                state++;//increment for the next state
+            }
 
+            //A wildcard symbol that matches any literal
+            else if (String.valueOf(p.charAt(j)).equals(".")) {
 
-            int t2=term(); //Remember the n2 of branch state
-            System.out.println("t1 "+t1+" t2 "+t2);
-
-            //Set the disjunction branch
-            setState(state,'`',t1,t2);
-
-            System.out.println("Current state with disjunction symbol: " + state + ", ch= " + character[state] + ", n1: " + n1[state] + ", n2: " + n2[state]);
-
-            //set the previous state from disjunction regex to disjunction branch
-            setState(f1-1,character[f1-1],state,state);
-
-            result=state;
-
-
-            //Check if preceding term is non-branching state by comparing two next states
-            if(n1[f1]==n2[f1]){
-
-                //Updating the preceding term's states
-                setState(f1,character[f1],state+1,state+1);
-
-            }else {
-                //Branching so only one state to merge
-                setState(f1,character[f1+1],n1[f1],state+1);
+                setState(state, String.valueOf(p.charAt(j + 1)), state + 1, state + 1);
+                j += 2;
+                result = state;
+                state++;//increment for the next state
 
             }
 
-            state++;
 
-            return result;
+            //Square brackets
+            else if (String.valueOf(p.charAt(j)).equals("[")) {
+
+                if (!String.valueOf(p.charAt(j + 1)).equals("]")) {
+                    String combinedLiterals = "";
+
+                    j++;
+                    while (!String.valueOf(p.charAt(j)).equals("]")) {
+
+                        combinedLiterals = combinedLiterals + combineCh(p.charAt(j));
+
+                        j++;
+                        //Reached the end of the string
+                        if (j == p.length()) {
+                            System.err.println("not a regular expression: no ']'");
+                            break;
+                        }
+                    }
+
+                    setState(state, combinedLiterals, state + 1, state + 1);
+
+                    j++;
+                    result = state;
+                    state++;
+
+                } else {
+                    System.err.println("Empty bracket");
+                }
+
+            } else if (String.valueOf(p.charAt(j)).equals("|")) {
+
+                //Disjunction branch
+                setState(state, "BR", t1, state + 1);
+
+                j++;
+                result = state;
+                state++;
+
+                int t2 = term(); //b, t2=4, t2's state is 4
+
+
+                System.out.println(t1);
+                //Check if preceding term is non-branching state by comparing two next states
+                if (n1[t1] == n2[t1]) {
+
+                    //Updating the preceding term's states
+                    setState(t1, character[t1], state, state);
+
+                } else {
+                    //Branching so only one state to merge
+                    setState(t1, character[t1], n1[t1], state);
+
+                }
+
+                return result;
+            }
+            //Factor itself
         }
-        //Factor itself
+        return result;
     }
-    return result;
-}
+
+
+    //All passed character is concatenated to form a string
+    public String combineCh(char ch) {
+
+        String stringChar = String.valueOf(ch);
+        ArrayList<String> charStrings = new ArrayList<String>();
+
+        charStrings.add(stringChar);
+        String sum = "";
+
+        for (int i = 0; i < charStrings.toArray().length; i++) {
+
+            sum = sum + "" + charStrings.get(i);
+        }
+
+        return sum;
+    }
 
 
     //find an expression (term, E)
-    public int expression(){
+    public int expression() {
 
-    int result =term();//find the term first
+        int result = term();//find the term first
 
-    //check if this is an end of string, null byte
-    if(p.charAt(j)==0){
-        //Only term in the expression
+        if (j < p.length()) {
+            //check if this is an end of string, null byte
+            if (p.charAt(j) == 0) {
+                //Only term in the expression
+                return result;
+
+            } else {
+                //Look ahead to see if there are more terms which starting with ( or literal
+                if (!String.valueOf(p.charAt(j)).equals("(") || isVocab(p.charAt(j))) {
+                    return result;
+                } else {
+                    expression();
+                }
+            }
+        }
+
         return result;
-
-    }else{
-        //Look ahead to see if there are more terms which starting with ( or literal
-        if(p.charAt(j)!='('||isVocab(p.charAt(j))){
-            return result;
-        }else {
-            expression();
-        }
     }
-    return result;
-}
 
-
-    public void dump(){
-        for (int i = 0; i<character.length;i++){
-            System.out.println("State: "+i+" ch is "+character[i]+" n1 is: "+n1[i]+" n2 is:"+n2[i]);
+    //Print out all the states in the FSM
+    public void dump() {
+        for (int i = 0; i < character.length; i++) {
+            System.out.println( i + " " + character[i] + " " + n1[i] + " " + n2[i]);
         }
 
     }
-
 }
